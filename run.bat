c02c5ee23d899c22bdeb5d4d284fc06ab573bbb2
Pras Kiven <arkiven4@gmail.com>
2025-08-01 09:03:34 +0900

    sws

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Change directory to where your script is located
+D:
+cd /d "D:\ITSTeam\NewGen\cbm_vale-main"
+
+REM Run the Python script with arguments
+python run.py --dataset CustomAWGN30ES15 --model "" --Device cpu --test
+
+REM Optional: pause to see output before window closes
+pause
